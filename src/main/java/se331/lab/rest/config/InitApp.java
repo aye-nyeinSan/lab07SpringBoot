9a80b2b78ff1ff3ddf212a96a753ca54914cbcf2
aye-nyeinSan <aye2904@gmail.com>
2024-10-06 00:16:47 +0700

    finished 3.2

--- src/main/java/se331/lab/rest/config/InitApp.java
+++ src/main/java/se331/lab/rest/config/InitApp.java
@@ -1,21 +1,36 @@
 package se331.lab.rest.config;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 import se331.lab.rest.entity.Event;
+import se331.lab.rest.entity.Organizer;
 import se331.lab.rest.repository.EventRepository;
+import se331.lab.rest.repository.OrganizerRepository;
 
 @Component
 @RequiredArgsConstructor
 
 public class InitApp implements ApplicationListener<ApplicationReadyEvent> {
+    @Autowired
     final EventRepository eventRepository;
+    final OrganizerRepository organizerRepository;
+
 
     @Override
+    @Transactional
     public void onApplicationEvent(ApplicationReadyEvent applicationReadyEvent) {
-        eventRepository.save(Event.builder()
+        Organizer org1,org2, org3;
+
+        org1 = organizerRepository.save(Organizer.builder().name("CAMT").build());
+        org2 = organizerRepository.save(Organizer.builder().name("CMU").build());
+        org3 = organizerRepository.save(Organizer.builder().name("Chiang Mai").build());
+
+        Event tempEvent;
+        tempEvent = eventRepository.save(Event.builder()
                 .category("Academic")
                 .title("Midterm Exam")
                 .description("A time for taking the exam ")
@@ -23,8 +38,11 @@ public class InitApp implements ApplicationListener<ApplicationReadyEvent> {
                 .date("3rd Sept")
                 .time("3.00-4.00 pm")
                 .petAllowed(false)
-                .organizer("CAMT").build());
-        eventRepository.save(Event.builder()
+                .build());
+        tempEvent.setOrganizer(org1);
+        org1.getOwnEvents().add(tempEvent);
+
+       tempEvent= eventRepository.save(Event.builder()
                 .category("Academic")
                 .title("Commencement Day")
                 .description("A time for celebration")
@@ -32,8 +50,11 @@ public class InitApp implements ApplicationListener<ApplicationReadyEvent> {
                 .date("21th Jan")
                 .time("8.am-4.00 pm ")
                 .petAllowed(false)
-                .organizer("CAMT").build());
-        eventRepository.save(Event.builder()
+                .build());
+       tempEvent.setOrganizer(org1);
+       org1.getOwnEvents().add(tempEvent);
+
+        tempEvent = eventRepository.save(Event.builder()
                 .category("Cultural")
                 .title("Loy Krathong")
                 .description("A time for Krathong")
@@ -41,8 +62,11 @@ public class InitApp implements ApplicationListener<ApplicationReadyEvent> {
                 .date("21th Nov")
                 .time("8.00-10.00 pm ")
                 .petAllowed(false)
-                .organizer("Chiang Mai").build());
-        eventRepository.save(Event.builder()
+                .build());
+        tempEvent.setOrganizer(org2);
+        org2.getOwnEvents().add(tempEvent);
+
+       tempEvent = eventRepository.save(Event.builder()
                 .category("Cultural")
                 .title("Songkran")
                 .description("Lets's Play Water")
@@ -50,7 +74,9 @@ public class InitApp implements ApplicationListener<ApplicationReadyEvent> {
                 .date("13th April")
                 .time("10.00am-6.00 pm ")
                 .petAllowed(true)
-                .organizer("Chiang Mai Municipality").build());
+                .build());
+       tempEvent.setOrganizer(org3);
+       org3.getOwnEvents().add(tempEvent);
     }
 
     @Override
--- src/main/java/se331/lab/rest/entity/Participant.java
+++ src/main/java/se331/lab/rest/entity/Participant.java
@@ -1,23 +1,23 @@
 package se331.lab.rest.entity;
+
 import jakarta.persistence.*;
 import lombok.*;
 
-import java.util.ArrayList;
 import java.util.List;
-
 @Data
 @Builder
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
-public class Organizer {
+public class Participant {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)//one table for organizer
     @EqualsAndHashCode.Exclude
     Long id;
     String name;
+    String tellNo;
 
-    @OneToMany (mappedBy = "organizer")
-            @Builder.Default
-    List<Event> ownEvents = new ArrayList<>();
+    @ManyToMany
+    List<Event> eventHistory;
 }
