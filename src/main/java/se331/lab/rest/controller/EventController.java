00e6226a7fa61ee31d264f3a249866e20e2f4086
aye-nyeinSan <aye2904@gmail.com>
2024-10-07 01:20:18 +0700

    finished lab10 1

--- src/main/java/se331/lab/rest/controller/EventController.java
+++ src/main/java/se331/lab/rest/controller/EventController.java
@@ -2,6 +2,7 @@ package se331.lab.rest.controller;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,12 +23,20 @@ public class EventController  {
     final EventService eventService;
     @GetMapping("/events")
     public ResponseEntity<?> getEventsLists(@RequestParam(value ="_limit",required = false) Integer perPage,
-                                            @RequestParam(value ="_page",required = false)Integer page) {
+                                            @RequestParam(value ="_page",required = false)Integer page,
+                                            @RequestParam(value="title",required = false) String title) {
 
 
-
-        Page<Event> pageOutput = eventService.getEvents(perPage,page);
+            perPage = perPage == null ? 3 : perPage;
+            page = page == null ? 1 : page;
+        Page<Event> pageOutput ;
+        if(title == null) {
+           pageOutput = eventService.getEvents(perPage,page);
         System.out.println("PagePoutput === "+pageOutput.getContent());
+        }
+        else {
+            pageOutput = eventService.getEvents(title, PageRequest.of(page -1,perPage));
+        }
 
         HttpHeaders responseHeader = new HttpHeaders();
         responseHeader.set("x-total-count",String.valueOf(pageOutput.getTotalElements()));
