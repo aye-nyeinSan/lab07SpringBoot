8875d0de33cb198feb5c602dd5867c128c0e95df
aye-nyeinSan <aye2904@gmail.com>
2024-10-08 22:08:28 +0700

    add DTO for participants

--- src/main/java/se331/lab/rest/entity/ParticipantEventDTO.java
+++ src/main/java/se331/lab/rest/entity/ParticipantEventDTO.java
@@ -1,4 +1,22 @@
 package se331.lab.rest.entity;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ParticipantEventDTO {
+    Long id;
+    String category;
+    String title;
+    String description;
+    String location;
+    String date;
+    String time;
+    Boolean petAllowed;
 }
